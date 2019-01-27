4acd372359ab8742fa6cd3ae7f2824f61609d69c
unknown <hassaa51@mcmaster.ca>
2019-01-26 20:21:48 -0500

    Merged functions into one class

--- ControlUnit.py
+++ ControlUnit.py
@@ -1,6 +1,7 @@
-import accelerator
+import csv
 import dangerousArea
 import datetime
+import math
 #import route.py
 class gps():
     def __init__(self):
@@ -13,16 +14,38 @@ class gps():
         now = datetime.datetime.now()
         currentDay = now.strftime("%a")
         if (currentDay == 'Sat' or currentDay == 'Sun' or punchedOut):
-            print("BIG UHOH")
+            self.triggerAccelerometer(long,lat)
         # do a dangerous area check
         if (dangerousArea.DangerousAreaCheck(long,lat)):
             print("Warning, entering dangerous area")
         # on route check
-        pass
+    def OnTrack(tupleThing):
+        longList = [1,2,3,4]
+        latList = [4,3,2,1]
+        i = 0
+        while i < len(longList):
+            if distance(longList[i]-long, latList[i]-lat, 1):
+                print("good")
+            else:
+                print("not good")
+            i+=1
+    def triggered(long, lat, lastLong, lastLat):
+        dx = lastLong - long
+        dy = lastLat - lat
+        if distance(dx, dy, 0.5):
+            print("good")
+        else:
+            print("not good")
+
+
     
+    def distance(dx, dy, r):
+        return math.sqrt(dx*dx + dy*dy) < r
+
+
     #just for show
     def triggerAccelerometer(self, long, lat):
-        print(accelerator.triggered(long, lat, self.lastLong, self.lastLat))
+        print(triggered(long, lat, self.lastLong, self.lastLat))
         if (currentDay == 'Sat' or currentDay == 'Sun' or punchedOut):
             print("GPS now turned on, getting location every 5 minutes")        
         
